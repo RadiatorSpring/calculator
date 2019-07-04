ce704903c66e84c1be3b5e39523e7e0285d609bd
I516445 <alexggeorgiev@abv.bg>
2019-07-04 13:34:33 +0300

    fixed tests

--- calculator-app/src/test/java/calculator/ParserTest.java
+++ calculator-app/src/test/java/calculator/ParserTest.java
@@ -40,8 +40,6 @@ public class ParserTest {
     public void testInfixToRPNConversionWithDifficultBrackets() {
         List<String> input = new ArrayList<>(Arrays.asList("2", "+", "2", "*", "(", "2", "-", "2", ")"));
         when(checker.isNumber("2")).thenReturn(true);
-//        when(checker.isOperationOrBracket("(")).thenReturn(true);
-//        when(checker.isOperationOrBracket(")")).thenReturn(true);
         when(checker.isOperationOrBracket("-")).thenReturn(true);
         when(checker.isOperationOrBracket("+")).thenReturn(true);
         when(checker.isOperationOrBracket("*")).thenReturn(true);
@@ -55,20 +53,26 @@ public class ParserTest {
 
     @Test
     public void testTextToInfixTrivial() {
-        String expression = "1/1";
+        String expression = "11/11";
 
-        List<String> expected = new ArrayList<>(Arrays.asList("1", "/", "1"));
-
+        List<String> expected = new ArrayList<>(Arrays.asList("11", "/", "11"));
+        when(checker.isOperationOrBracket('/')).thenReturn(true);
+        when(checker.isNumber('1')).thenReturn(true);
         List<String> actual = parser.textToInfix(expression);
 
-        Assert.assertEquals("expected to return list with elements [1] [+] [1]", expected, actual);
+        Assert.assertEquals("expected to return list with elements [11] [+] [11]", expected, actual);
     }
 
     @Test
     public void testTextToInfixLongExpression() {
         String expression = "2+2*(2-2)";
         List<String> expected = new ArrayList<>(Arrays.asList("2", "+", "2", "*", "(", "2", "-", "2", ")"));
-
+        when(checker.isNumber('2')).thenReturn(true);
+        when(checker.isOperationOrBracket('*')).thenReturn(true);
+        when(checker.isOperationOrBracket('-')).thenReturn(true);
+        when(checker.isOperationOrBracket('+')).thenReturn(true);
+        when(checker.isOperationOrBracket('(')).thenReturn(true);
+        when(checker.isOperationOrBracket(')')).thenReturn(true);
         List<String> actual = parser.textToInfix(expression);
 
         Assert.assertEquals("expected list with 2+2*(2-2) each element at different index", expected, actual);
