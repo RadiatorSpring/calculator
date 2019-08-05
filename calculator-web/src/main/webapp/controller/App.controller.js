1d46d6b31626c7d61268fc6e9ae25d9cff5c6cf0
RadiatorSpring <alexggeorgiev@abv.bg>
2019-08-05 14:48:00 +0300

    added walkthrough classes

--- calculator-web/src/main/webapp/controller/App.controller.js
+++ calculator-web/src/main/webapp/controller/App.controller.js
@@ -1,16 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+ ], function (Controller) {
 	"use strict";
-
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-		onShowHello : function () {
-			// show a native JavaScript alert
-			/* eslint-disable no-alert */
-			alert("Hello World");
-			/* eslint-enable no-alert */
-		}
 	});
-
-});
+ });	
