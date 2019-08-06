0466e42bdf0cb8c8ae0d610953e27e8db15f7d05
RadiatorSpring <alexggeorgiev@abv.bg>
2019-08-06 11:12:01 +0300

    not working walkthrough

--- calculator-web/src/main/webapp/controller/App.controller.js
+++ calculator-web/src/main/webapp/controller/App.controller.js
@@ -1,8 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
- ], function (Controller) {
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
 	"use strict";
+
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+		onOpenDialog: function () {
+			this.getOwnerComponent().openHelloDialog();
+		}
+		
 	});
- });	
+
+});
