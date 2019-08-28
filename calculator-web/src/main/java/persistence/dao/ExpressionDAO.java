0ab871f74eb5bfb636187fdf994a51919ea49beb
Georgiev <aleksandar.georgiev@sap.com>
2019-08-28 15:28:37 +0300

    Fix missing semicolon

--- calculator-web/src/main/java/persistence/dao/ExpressionDAO.java
+++ calculator-web/src/main/java/persistence/dao/ExpressionDAO.java
@@ -1,8 +1,30 @@
-package persistence;
+package persistence.dao;
+
+import persistence.dto.ExpressionDTO;
+
+import javax.persistence.*;
 
 public class ExpressionDAO {
 
-    public void saveExpression(String expression,double result){
-        ExpressionDTO expressionDTO = new ExpressionDTO()
+
+    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("expression");
+    private EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    public void save(ExpressionDTO expressionDTO) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(expressionDTO);
+        entityManager.getTransaction().commit();
     }
+
+    public void delete(ExpressionDTO expressionDTO) {
+        entityManager.getTransaction().begin();
+        entityManager.remove(expressionDTO);
+        entityManager.getTransaction().commit();
+    }
+
+    public ExpressionDTO getExpression(int id) {
+        return entityManager.find(ExpressionDTO.class, id);
+    }
+
+
 }
