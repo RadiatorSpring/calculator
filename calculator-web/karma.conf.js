1bcf36562995e4e94376bcaf44871bd48821a494
Georgiev <aleksandar.georgiev@sap.com>
2019-09-05 14:38:41 +0300

    added dependency for karma

--- calculator-web/karma.conf.js
+++ calculator-web/karma.conf.js
@@ -6,13 +6,15 @@ module.exports = function (config) {
         ui5: {
             type: "application",
             paths: {
-                webapp: "src/main/webapp"
+                webapp: "webapp"
             },
             url: "https://localhost:8080",
             testpage: "src/main/webapp/test/integration/opa.html"
+
+
         },
+	singleRun:true,
         browsers: ["Chrome"]
 
     });
 };
-//./node_modules/karma/bin/karma start
