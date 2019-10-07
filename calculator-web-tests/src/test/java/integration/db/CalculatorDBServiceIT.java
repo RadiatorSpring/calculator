50605d758ba76f994bcd0bc00724554d2e32ca06
Georgiev <aleksandar.georgiev@sap.com>
2019-10-07 14:04:16 +0300

    added integration tests for db

--- calculator-web-tests/src/test/java/integration/db/CalculatorDBServiceIT.java
+++ calculator-web-tests/src/test/java/integration/db/CalculatorDBServiceIT.java
@@ -1,4 +1,48 @@
 package integration.db;
 
+import integration.db.page.CalculationPage;
+import integration.web.page.CorrectResponsePage;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import persistence.dao.ExpressionResultDAO;
+import persistence.dto.ExpressionResultDTO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class CalculatorDBServiceIT {
+
+    private EntityManager entityManager;
+    Logger logger = LoggerFactory.getLogger(CalculatorDBServiceIT.class);
+
+    @Before
+    public void setEntityManager() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @Test
+    public void testSimpleRequest() throws IOException {
+        String expression = "1-11";
+        CorrectResponsePage correctResponsePage = new CorrectResponsePage(CalculationPage.LOCAL_URL + expression);
+//        CalculationPage calculationPage = new CalculationPage(expression);
+//        calculationPage.calculate();
+
+        ExpressionResultDTO foundDTO = entityManager.find(ExpressionResultDTO.class, 1L);
+        logger.info("this is the expressionDTO -> " + foundDTO);
+        logger.info("this is the entityManager -> " + entityManager);
+
+        assertEquals(foundDTO.getId(), 1);
+        assertNull(foundDTO.getError());
+        assertEquals(foundDTO.getExpression(), expression);
+        assertEquals(foundDTO.getEvaluation(), -10.0, 0.01);
+
+    }
 }
