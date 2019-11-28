7f35eee373e1dbb5b18438b7758ef61588c8ca3d
I516445 <alexggeorgiev@abv.bg>
2019-11-28 15:32:37 +0200

    Remove redundant declarations

--- calculator-web/src/test/java/persistence/ExpressionDAOSaveTest.java
+++ calculator-web/src/test/java/persistence/ExpressionDAOSaveTest.java
@@ -15,26 +15,22 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 @RunWith(JpaUnitRunner.class)
-public class DAOSaveTest {
+public class ExpressionDAOSaveTest {
     private static final String PERSISTENCE_UNIT_NAME = "test";
 
     @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
     private EntityManager entityManager;
     private ExpressionResultDAO expressionResultDAO;
-    private CalculationsDAO calculationsDAO;
 
     @Before
     public void setup() {
-        calculationsDAO = new CalculationsDAO(PERSISTENCE_UNIT_NAME);
         expressionResultDAO = new ExpressionResultDAO(PERSISTENCE_UNIT_NAME);
     }
-
 
     @Test
     public void testSave() {
         long testingId = 1L;
         String expression = "1-1";
-        double evaluation = 0;
 
         ExpressionResultDTO savedDTO = new ExpressionResultDTO(expression, true);
         expressionResultDAO.save(savedDTO);
